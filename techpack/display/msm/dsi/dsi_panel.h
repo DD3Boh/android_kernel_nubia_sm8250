d221e95abd5597cad69eb7d75dc2eb164b622a69
Demon000 <demonsingur@gmail.com>
2020-08-18 01:11:56 +0200

    drm: msm: implement fod handling

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -173,6 +173,12 @@ struct drm_panel_esd_config {
 	u32 groups;
 };
 
+#define BRIGHTNESS_ALPHA_PAIR_LEN 2
+struct brightness_alpha_pair {
+	u32 brightness;
+	u32 alpha;
+};
+
 struct dsi_panel {
 	const char *name;
 	const char *type;
@@ -226,6 +232,9 @@ struct dsi_panel {
 	int panel_test_gpio;
 	int power_mode;
 	enum dsi_panel_physical_type panel_type;
+
+	struct brightness_alpha_pair *fod_dim_lut;
+	u32 fod_dim_lut_count;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -352,5 +361,9 @@ int dsi_panel_read_data(struct mipi_dsi_device *dsi, u8 cmd, void* buf, size_t l
 int dsi_panel_write_data(struct mipi_dsi_device *dsi, u8 cmd, void* buf, size_t len);
 void nubia_write_panel_osc_timing(uint32_t dfps, struct mipi_dsi_device *dsi);
 #endif
+
+int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
+
+u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
