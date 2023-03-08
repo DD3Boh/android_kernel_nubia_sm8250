5e5d1b7792c03f353f064b2cb1785629df3979dc
Davide Garberi <dade.garberi@gmail.com>
2023-03-08 19:16:57 +0100

    drivers: firmware: tz_log: Increase Qsee log buf size

--- drivers/firmware/qcom/tz_log.c
+++ drivers/firmware/qcom/tz_log.c
@@ -22,8 +22,10 @@
 #include <soc/qcom/qseecomi.h>
 #include <soc/qcom/qtee_shmbridge.h>
 
-/* QSEE_LOG_BUF_SIZE = 32K */
-#define QSEE_LOG_BUF_SIZE 0x8000
+///* QSEE_LOG_BUF_SIZE = 32K */
+//#define QSEE_LOG_BUF_SIZE 0x8000
+/* QSEE_LOG_BUF_SIZE = 256K */
+#define QSEE_LOG_BUF_SIZE 0x40000
 
 
 /* TZ Diagnostic Area legacy version number */
